5d643343ae270dc2baf8d1ff3252d1268478ae7a
hongshunli-spec <hongshunli@gmail.com>
2026-08-14 11:46:10 +0800

    澶嶅埗鏂板鏂囩珷

--- pub.bat
+++ pub.bat
@@ -1,19 +1,3 @@
-@echo off
-chcp 65001
-cd /d D:\myblog
-echo 拉取云端最新代码防止冲突
-git pull origin main
-echo.
-echo 生成静态页面
-hugo
-echo.
-echo 提交全部修改文件
 git add .
-set /p msg=输入更新备注(不能为空):
-git commit -m "%msg%"
-echo.
-echo 推送至GitHub
+git commit -m "复制新增文章"
 git push origin main
-echo.
-echo ========== 发布完成 ==========
-pause
